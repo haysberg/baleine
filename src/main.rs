07cb53d20c11058bac6b889c5c61c25d0125a4ad
Téo Haÿs <teo.hays@protonmail.com>
2021-09-10 12:00:38 +0200

    Added args.yaml and clap to manage arguments and create a useful help command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+#[macro_use]
+extern crate clap;
+use clap::{Arg, App, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+    let yaml = load_yaml!("args.yaml");
+    let matches = App::from_yaml(yaml).get_matches();
+
+    // You can handle information about subcommands by requesting their matches by name
+    // (as below), requesting just the name used, or both at the same time
+    if let Some(matches) = matches.subcommand_matches("test") {
+        if matches.is_present("debug") {
+            println!("Printing debug info...");
+        } else {
+            println!("Printing normally...");
+        }
+    }
 }
