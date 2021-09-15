5471385c2c77d14547a98762d7034fab36b61bab
Téo Haÿs <teo.hays@protonmail.com>
2021-09-15 09:32:56 +0200

    Moved to dotenv successfully. Added the option to display the help message whenever you run the program without any subcommand.

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,25 @@
 #[macro_use]
-extern crate clap;
-
-use clap::{App};
 mod save;
 mod deploy;
 mod list;
 mod destroy;
 
+extern crate dotenv;
+use dotenv::dotenv;
+
 fn main() {
     //We get the arguments provided by the user, and match them with the ones listed in args.yaml
-    let app_yaml = load_yaml!("../args.yaml");
-    let matches = App::from_yaml(app_yaml).get_matches();
+    let app_yaml = clap::load_yaml!("../args.yaml");
+    let matches = clap::App::from_yaml(app_yaml).get_matches();
 
-    //Loading the settings from config.toml
-    let mut config = config::Config::default();
-    match config.merge(config::File::with_name("../config.toml")){
-        Ok(_) => (),
-        Err(err) => println!("{}", err),
-    }
+    dotenv().ok();
 
     //Depending on what subcommand the user has put in the CLI, we call the related function.
     match matches.subcommand_name() {
         Some("deploy") => deploy::deploy(matches.subcommand_matches("deploy")),
         Some("destroy") => destroy::destroy(matches.subcommand_matches("destroy")),
-        Some("list") => list::list(&config),
-        Some("save") => save::save(matches.subcommand_matches("save"), &config),
+        Some("list") => list::list(),
+        Some("save") => save::save(matches.subcommand_matches("save")),
         None => println!("You need to put a subcommand for r2dock to work"),
         _ => unreachable!(),
     }
