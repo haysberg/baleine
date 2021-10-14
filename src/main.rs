aa00768e57d994f08a7cd162015acb731f3eb133
Téo Haÿs <teo.hays@protonmail.com>
2021-10-14 11:06:00 +0200

    Better clarity on websocket connection

--- src/main.rs
+++ src/main.rs
@@ -5,28 +5,32 @@ mod deploy;
 mod list;
 mod destroy;
 mod utils;
-
+use std::thread;
 extern crate dotenv;
-use crate::utils::list_of_images;
+use crate::utils::*;
 use dotenv::dotenv;
 
 extern crate serde_derive;
 extern crate serde_json;
 
 fn main() {
+    dotenv().ok();
+
+    thread::spawn(|| {
+        loop {
+            update_nodes_state();
+        }
+    });
+
     //We get the arguments provided by the user, and match them with the ones listed in args.yaml
     let app_yaml = clap::load_yaml!("../args.yaml");
-    let matches = clap::App::from_yaml(app_yaml).get_matches();
-
-    list_of_images();
-
-    dotenv().ok();
+    let matches = clap::App::from_yaml(app_yaml).get_matches();    
 
     //Depending on what subcommand the user has put in the CLI, we call the related function.
     match matches.subcommand_name() {
         Some("deploy") => deploy::entry(&matches),
         Some("destroy") => destroy::entry(&matches),
-        Some("list") => list::list(matches.subcommand_matches("list")),
+        Some("list") => list::entry(&matches),
         Some("save") => save::entry(matches.subcommand_matches("save")),
         None => println!("You need to put a subcommand for r2dock to work"),
         _ => unreachable!(),
--- src/utils.rs
+++ src/utils.rs
@@ -1,65 +1,59 @@
-use tungstenite::{connect, Message};
-use url::Url;
+use tungstenite::Message;
+use openssl::ssl::{SslConnector, SslMethod};
+use std::net::TcpStream;
+use std::env;
 
-#[derive(Default, Debug, Clone, PartialEq, serde_derive::Serialize, serde_derive::Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Root {
-    pub category: String,
-    pub action: String,
-    pub r2lab_state: Vec<R2LabState>,
-}
+static mut COUNT : u8 = 0;
 
-#[derive(Default, Debug, Clone, PartialEq, serde_derive::Serialize, serde_derive::Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct R2LabState {
-    #[serde(rename = "gnuradio_release")]
-    pub gnuradio_release: String,
-    pub uname: String,
-    #[serde(rename = "usrp_on_off")]
-    pub usrp_on_off: String,
-    #[serde(rename = "os_release")]
-    pub os_release: String,
-    #[serde(rename = "control_ping")]
-    pub control_ping: String,
-    #[serde(rename = "cmc_on_off")]
-    pub cmc_on_off: String,
-    #[serde(rename = "image_radical")]
-    pub image_radical: String,
-    pub id: i64,
-    pub available: Option<String>,
-    #[serde(rename = "control_ssh")]
-    pub control_ssh: String,
-    #[serde(rename = "images_usrp")]
-    #[serde(default)]
-    pub images_usrp: Vec<::serde_json::Value>,
-    #[serde(rename = "images_wifi")]
-    pub images_wifi: Option<Vec<String>>,
-    #[serde(rename = "usrp_type")]
-    pub usrp_type: Option<String>,
-    #[serde(rename = "usrp_duplexer")]
-    pub usrp_duplexer: Option<String>,
-    #[serde(default)]
-    pub images: Vec<String>,
-}
+pub fn update_nodes_state(){
 
-pub fn list_of_images(){
+    let mut ctx = SslConnector::builder(SslMethod::tls_client()).unwrap();
+    println!("A");
+    ctx.set_verify(openssl::ssl::SslVerifyMode::NONE); // <- verification disabled
+    println!("B");
+    let connector = ctx.build();
+    println!("C");
+    let stream = TcpStream::connect(("r2lab.inria.fr", 999)).unwrap();
+    println!("D");
+    let stream = connector.connect("r2lab.inria.fr", stream).unwrap();
 
-    let (mut socket, response) =
-        connect(Url::parse("wss://r2lab.inria.fr:999").unwrap()).expect("Can't connect");
+    let mut client = tungstenite::client_tls("ws://r2lab.inria.fr:999", stream).unwrap();
 
-    println!("Connected to the server");
-    println!("Response HTTP code: {}", response.status());
-    println!("Response contains the following headers:");
-    for (ref header, _value) in response.headers() {
-        println!("* {}", header);
-    }
+    println!("Connected to the R2Lab monitoring server. Waiting for a status message...");
 
-    socket.write_message(Message::text("{category: \"nodes\", action: \"request\", message: \"please\"}").into()).unwrap();
     loop {
-        let msg = socket.read_message().expect("Error reading message");
-        println!("Received: {}", msg);
+        println!("Loop");
+        match client.0.write_message(Message::text("{category: \"nodes\", action: \"request\", message: \"please\"}")){
+            Ok(_) => match client.0.read_message().unwrap().to_text(){
+                    Ok(res) => set_env_json(res.to_string()),
+                    Err(_) => (println!("Fail"))
+                },
+            Err(_) => panic!("Problem while getting the list of nodes !")
+        };
     }
 }
 
+pub fn set_env_json(json_str : String){
+    unsafe {
+        COUNT = COUNT + 1;
+        println!("{}", COUNT);
+    }
+    env::set_var("MONITOR_NODES", json_str);
+    match env::var("MONITOR_NODES") {
+        Ok(val) => println!("{}: {:?}", "MONITOR_NODES", val),
+        Err(e) => println!("couldn't interpret {}: {}", "MONITOR_NODES", e),
+    }
+}
 
-//wss://r2lab.inria.fr:999
+pub fn lock(){
+    loop {
+        match env::var("MONITOR_NODES") {
+            Ok(val) => {
+                if val != "" {
+                    break
+                }
+            }, 
+            Err(_) => ()
+        }
+    }
+}
