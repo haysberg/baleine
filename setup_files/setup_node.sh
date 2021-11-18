72f9a35c412a8dad1afea9b8d216a62dfc485711
Téo Le Dozo <teo.hays@protonmail.com>
2021-11-18 11:54:57 +0100

    Update setup_node.sh

--- setup_files/setup_node.sh
+++ setup_files/setup_node.sh
@@ -3,13 +3,13 @@
 #Install Docker
 apt-get remove docker docker-engine docker.io containerd runc -y
 apt update -y
-apt install ca-certificates curl gnupg lsb-release
+apt install ca-certificates curl gnupg lsb-release -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-apt update
+apt update -y
 apt install docker-ce docker-ce-cli containerd.io -y
 
 #Create a new user and give it permission to use Docker even if not root
@@ -27,5 +27,8 @@ echo $insecure | tee -a /etc/docker/daemon.json > /dev/null
 echo "docker exec -i container \"\$@\"" | tee /bin/r2 > /dev/null
 chmod +x /bin/r2
 
+echo "/bin/r2" | tee /etc/shells > /dev/null
+
+chsh --shell /bin/sh tecmint
 
 #docker volume create --driver local --opt type=tmpfs --opt device=:/home/container:bind_mount r2dock-bind-mount
